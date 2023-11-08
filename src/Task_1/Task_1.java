9810eb4867f9ffad49abc08d3d4ce15bd7ca7d1f
Fedor-Fender <developerc26java@gmail.com>
2023-11-08 21:32:46 +0300

    First task

---
+++ src/Task_1/Task_1.java
@@ -0,0 +1,9 @@
+package Task_1;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        double a = 1245.578;
+        int b = (int) a;
+        System.out.println("Output data with new type is " +b);
+    }
+}
