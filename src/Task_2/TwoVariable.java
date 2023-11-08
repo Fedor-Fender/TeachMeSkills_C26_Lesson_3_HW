e05598bff6a0d8263f0509ba9972fcb8e4f5e36b
Fedor-Fender <developerc26java@gmail.com>
2023-11-08 21:37:10 +0300

    Second task

---
+++ src/Task_2/TwoVariable.java
@@ -0,0 +1,12 @@
+package Task_2;
+
+public class TwoVariable {
+    public static void main(String[] args) {
+        char a = 1;
+        int b = 1;
+        System.out.println("Output sum = " + (a + b));
+        /*type char has value=1 and type int has value=1.
+        *both of them is integral meaning so we can add
+        */
+    }
+}
