7eb4dc67dc21b22f909cc80f350bc12a6fd08e43
Fedor-Fender <developerc26java@gmail.com>
2023-11-09 11:04:50 +0300

    Second task

--- src/Task_1/Task_1.java
+++ src/Task_1/Task_1.java
@@ -5,5 +5,6 @@ public class Task_1 {
         double a = 1245.578;
         int b = (int) a;
         System.out.println("Output data with new type is " +b);
+        System.out.println("Output data with new type is " +b);
     }
 }
