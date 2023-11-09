3412995a7dea664af2551dfd8d44e5a3d749f62d
Fedor-Fender <developerc26java@gmail.com>
2023-11-09 11:24:51 +0300

    fifth task

---
+++ src/Task_5/SquaredValue.java
@@ -0,0 +1,9 @@
+package Task_5;
+
+public class SquaredValue {
+    public static void main(String[] args) {
+        for (int i = 10; i <=20 ; i++) {
+            System.out.println(i*i);
+        }
+    }
+}
