ece8a9f80af6139deef87e0a85a4bc928673f2c0
Fedor-Fender <developerc26java@gmail.com>
2023-11-09 11:27:03 +0300

    sixth task

---
+++ src/Task_6/Number.java
@@ -0,0 +1,11 @@
+package Task_6;
+
+public class Number {
+    public static void main(String[] args) {
+        int num=7;
+        while (num<99){
+            System.out.println("Output "+num);
+            num = num + 7;
+        }
+    }
+}
